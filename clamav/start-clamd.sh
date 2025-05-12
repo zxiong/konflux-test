12b85aafcc51a74c585fcf0b7671f65f183aaeb4
Zhiming Xiong <zxiong@redhat.com>
2025-05-12 19:50:21 +0800

    Add entrypoint

---
+++ clamav/start-clamd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start clamd in background
+echo "[+] Starting clamd..."
+/usr/sbin/clamd -c /etc/clamd.d/scan.conf --foreground=false &
+
+# Wait for the socket to be created
+echo "[+] Waiting for clamd to be ready..."
+for i in {1..30}; do
+    if [ -S /var/run/clamd.scan/clamd.sock ]; then
+        echo "[+] clamd is ready!"
+        break
+    fi
+    sleep 1
+done
+
+# If command arguments are provided, run them
+if [ $# -gt 0 ]; then
+    echo "[+] Running: $@"
+    exec "$@"
+fi
